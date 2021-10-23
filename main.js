87e4f035d02d5ba67c882a2850d98cf6c0b41789
DexREAU <andromedog2017@gmail.com>
2021-10-23 19:23:48 -0300

    2

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ document.body.onscroll = function() {
     windowPosition = window.pageYOffset;
     parallaxImg.style.transform = "translateY( -" + windowPosition * 1.25 + "px)";
 }
+
+function success() {
+    const button = document.querySelector('form button');
+    const input = document.querySelector('form input');
+
+    button.innerText = "OBRIGADO!"
+
+    inputs.value = ""
+
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    let myForm = document.getElementById('form');
+    let formData = new FormData(myForm)
+    fetch('/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString()
+    }).then(() => success()).catch((error) =>
+      alert(error))
+  }
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
